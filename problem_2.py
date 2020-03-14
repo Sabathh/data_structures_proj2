b2765ae23d9516ea31ec657eacb099231f192917
Tulio Di Sabatino <Tlio Di Sabatino>
2020-03-14 22:44:19 +0000

    Problem 2: Implemented find_files function using recursion

--- problem_2.py
+++ problem_2.py
@@ -1,3 +1,6 @@
+import os
+from typing import List
+
 def find_files(suffix, path):
     """
     Find all files beneath path with file name suffix.
@@ -14,4 +17,52 @@ def find_files(suffix, path):
     Returns:
        a list of paths
     """
-    return None
+    list_of_files = []
+
+    recursive_find_files(suffix, path, list_of_files)
+
+    return list_of_files
+
+def recursive_find_files(suffix : str, path : str, list_of_files : List[str]):
+  """ Recursively find files containing suffix and append to list_of_files
+  
+  Arguments:
+      suffix {str} -- File extension 
+      path {str} -- Path of directory/file to be searched
+      list_of_files {List[str]} -- List of strings that shall contain the result
+  """
+  # Base case: If a path to a file is provided, return
+  if os.path.isfile(path):
+    if path.endswith(suffix):
+      list_of_files.append(path)
+    return
+
+  # If directory path is provided then call recursive_find_files once for each item in directory
+  for item in os.listdir(path):
+    item_path = os.path.join(path, item)
+    recursive_find_files(suffix, item_path, list_of_files)
+
+def test_find_files():
+  # Single folder containing one file to be found
+  assert(find_files(".c", ".\\problem_2_dir\\subdir1") == ['.\\problem_2_dir\\subdir1\\a.c'])
+  # Single folder containing no file to be found
+  assert(find_files(".c", ".\\problem_2_dir\\subdir2") == [])
+  # Deep path containing multiple files to be found
+  assert(find_files(".c", ".\\problem_2_dir\\deepdir") == ['.\\problem_2_dir\\deepdir\\deepdir1\\deepdir1_1\\deepdir1_1_1\\a.c',
+                                                           '.\\problem_2_dir\\deepdir\\deepdir1\\deepdir1_1\\deepdir1_1_1\\b.c',
+                                                           '.\\problem_2_dir\\deepdir\\deepdir1\\deepdir1_1\\deepdir1_1_1\\c.c'])
+  # Full test
+  assert(find_files(".c", ".\\problem_2_dir") == ['.\\problem_2_dir\\deepdir\\deepdir1\\deepdir1_1\\deepdir1_1_1\\a.c',
+                                                  '.\\problem_2_dir\\deepdir\\deepdir1\\deepdir1_1\\deepdir1_1_1\\b.c',
+                                                  '.\\problem_2_dir\\deepdir\\deepdir1\\deepdir1_1\\deepdir1_1_1\\c.c',
+                                                  '.\\problem_2_dir\\subdir1\\a.c', 
+                                                  '.\\problem_2_dir\\subdir3\\subsubdir1\\b.c', 
+                                                  '.\\problem_2_dir\\subdir5\\a.c',
+                                                  '.\\problem_2_dir\\t1.c'])
+
+if __name__ == "__main__":
+  
+  test_find_files()
+
+  print("Tests completed!")
+
