cb87a8026f0c56aec9a05183a2ba20d2bdb75157
Tulio Di Sabatino <Tlio Di Sabatino>
2020-03-16 22:48:51 +0000

    Problem 6: Added testcases. Added comments

--- problem_6.py
+++ problem_6.py
@@ -42,7 +42,13 @@ class LinkedList:
         return size
 
 def list_to_set(llist : LinkedList) -> set:
-
+    """ Converts a LinkedList into a set
+    
+    Arguments:
+        llist {LinkedList} -- LinkedList
+    Returns:
+        set -- Set containing all elements of llist
+    """
     current_node = llist.head
     lset = set()
     while current_node is not None:
@@ -52,6 +58,14 @@ def list_to_set(llist : LinkedList) -> set:
     return lset
 
 def set_to_list(lset : set) -> LinkedList:
+    """ Converts a set into a LinkedList
+    
+    Arguments:
+        lset {set} -- Set to be converted
+    
+    Returns:
+        LinkedList -- LinkedList containing all elements of set
+    """
     llist = LinkedList()
 
     for elem in lset:
@@ -60,7 +74,16 @@ def set_to_list(lset : set) -> LinkedList:
     return llist
 
 
-def union(llist_1, llist_2):
+def union(llist_1 : LinkedList, llist_2 : LinkedList) -> LinkedList:
+    """ Uses sets to generate a LinkedList containing the union of llist_1 and llist_2
+    
+    Arguments:
+        llist_1 {LinkedList} -- LinkedList object
+        llist_2 {LinkedList} -- LinkedList object
+    
+    Returns:
+        LinkedList -- LinkedList containing the union of inputs
+    """
     lset_1 = list_to_set(llist_1)
     lset_2 = list_to_set(llist_2)
     
@@ -68,7 +91,16 @@ def union(llist_1, llist_2):
 
     return set_to_list(union_set)
 
-def intersection(llist_1, llist_2):
+def intersection(llist_1 : LinkedList, llist_2 : LinkedList) -> LinkedList:
+    """ Uses sets to generate a LinkedList containing the intersection of llist_1 and llist_2
+    
+    Arguments:
+        llist_1 {LinkedList} -- LinkedList object
+        llist_2 {LinkedList} -- LinkedList object
+    
+    Returns:
+        LinkedList -- LinkedList containing the intersection of inputs
+    """
     lset_1 = list_to_set(llist_1)
     lset_2 = list_to_set(llist_2)
     
@@ -76,38 +108,55 @@ def intersection(llist_1, llist_2):
 
     return set_to_list(intersec_set)
 
-# /TODO: Add test cases
+def test_edge_cases():
+    pass
 
-# Test case 1
+def test_standard_lists(element_1 : list, element_2 : list):
 
-linked_list_1 = LinkedList()
-linked_list_2 = LinkedList()
+    linked_list_1 = LinkedList()
+    linked_list_2 = LinkedList()
 
-element_1 = [3,2,4,35,6,65,6,4,3,21]
-element_2 = [6,32,4,9,6,1,11,21,1]
+    for i in element_1:
+        linked_list_1.append(i)
 
-for i in element_1:
-    linked_list_1.append(i)
+    for i in element_2:
+        linked_list_2.append(i)
 
-for i in element_2:
-    linked_list_2.append(i)
+    union_list = union(linked_list_1,linked_list_2)
+    intersec_list = intersection(linked_list_1,linked_list_2)
 
-print (union(linked_list_1,linked_list_2))
-print (intersection(linked_list_1,linked_list_2))
+    node = union_list.head
+    if node is None:
+        # Triggered when both lists are empty
+        assert(len(element_1) == 0 and len(element_2) == 0)
+    else:
+        while node is not None:
+            assert(node.value in element_1+element_2)
+            node = node.next
 
-# Test case 2
+    node = intersec_list.head
+    if node is None:
+        # Triggered when intersection is empty
+        intersec_set = set(element_1).intersection(set(element_2))
+        assert(len(intersec_set) == 0)
+    else:
+        while node is not None:
+            assert(node.value in element_1 and node.value in element_2)
+            node = node.next
 
-linked_list_3 = LinkedList()
-linked_list_4 = LinkedList()
+if __name__ == "__main__":
+    test_edge_cases()
 
-element_1 = [3,2,4,35,6,65,6,4,3,23]
-element_2 = [1,7,8,9,11,21,1]
+    element_1 = [3,2,4,35,6,65,6,4,3,21]
+    element_2 = [6,32,4,9,6,1,11,21,1]
+    test_standard_lists(element_1, element_2)
 
-for i in element_1:
-    linked_list_3.append(i)
+    element_3 = [3,2,4,35,6,65,6,4,3,23]
+    element_4 = [1,7,8,9,11,21,1]
+    test_standard_lists(element_3, element_4)
 
-for i in element_2:
-    linked_list_4.append(i)
+    element_5 = []
+    element_6 = []
+    test_standard_lists(element_5, element_6)
 
-print (union(linked_list_3,linked_list_4))
-print (intersection(linked_list_3,linked_list_4))
+
